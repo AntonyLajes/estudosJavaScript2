6c65c5ef9b3513a282ba3f3359a290f5662c4ebf
Antony Mateus Coelho Lajes <antonym@assessorpublico.local>
2021-07-14 16:02:28 -0300

    Fim TODOLIST/seção12

--- TODOLIST/app.js
+++ TODOLIST/app.js
@@ -6,6 +6,8 @@ const botaoCadastra = document.querySelector("botao_adc");
 const paleta1 = document.querySelector("#paleta1");
 const paleta2 = document.querySelector("#paleta2");
 const paleta3 = document.querySelector("#paleta3");
+const body = document.getElementsByTagName("body");
+const delTodos = document.querySelector(".botao_del_todos");
 
 /*
 console.log(container);
@@ -23,10 +25,25 @@ console.log(paleta3);
 
 
 
+//CRIANDO ELEMENTOS
+function criaAtividade(){
+    const atividade = document.createElement("div");
+    atividade.classList.add("atividade");
+    const nomeAtividade = document.createElement("p");
+    atividade.textContent = input.value;
+    const botaoLimpar = document.createElement("button");
+    botaoLimpar.textContent = "Limpar";
+    botaoLimpar.classList.add("botao_del");
+    botaoLimpar.addEventListener('click', () => removeAtividadeSelecionada(atividade));
+    listaDeAtividades.appendChild(atividade);
+    atividade.appendChild(nomeAtividade);
+    atividade.appendChild(botaoLimpar); 
+}
+
 function cadastraAtividade(){
     if(input.value.length > 3){
         erro.style.display = "none";
-        //criaAtividade();
+        criaAtividade();
     }else{
         erro.style.display = "grid";
         erro.innerHTML = `${input.value} não é uma atividade válida!`
@@ -36,6 +53,31 @@ function cadastraAtividade(){
 
 function limpaInput(){
     input.value = "";
+}
+
+paleta1.addEventListener("click", () => alteraCorDeFundo('seagreen')) //adicionador de eventos, assim ele nao precisa estar em contato direto com o html evitando futuros bugs
+paleta2.addEventListener('click', () => alteraCorDeFundo('slateblue'));
+paleta3.addEventListener('click', () => alteraCorDeFundo('tomato'));
+
+delTodos.addEventListener('click', () => removeAtividades());
+
+function alteraCorDeFundo(cor){
+    container.style.background = cor;
+    listaDeAtividades.style.background = cor;
+}
+
+function addNoBotaoMais(){
+    cadastraAtividade();
+}
+
+function removeAtividadeSelecionada(atividade){
+    listaDeAtividades.removeChild(atividade)
+}
+
+function removeAtividades(){
+    while(listaDeAtividades.firstElementChild){
+        listaDeAtividades.removeChild(listaDeAtividades.firstElementChild);
+    }
 }
 
 window.addEventListener("keypress", (e) => {
